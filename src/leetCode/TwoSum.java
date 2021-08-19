9f42d2dc5868b018bc31872f87ae3ae1ba0a5ad9
arpanechoindia <70320733+arpanechoindia@users.noreply.github.com>
2021-08-19 16:39:30 +0530

    TwoSum solution, Time Complexity O(n)

--- src/leetCode/TwoSum.java
+++ src/leetCode/TwoSum.java
@@ -1,20 +1,6 @@
-package leetCode;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class TwoSum {
-
-	public static void main(String[] args) {
-		int[] nums = new int[]{3,3};
-		int target = 6;
-		int[] result = new int[] {};
-		result = twoSum(nums, target);
-		System.out.println(result[0]);
-		System.out.println(result[1]);
-	}
-	 public static int[] twoSum(int[] nums, int target) {
-	        int[] result = new int[2];
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       int[] result = new int[2];
 	    Map<Integer, Integer> map = new HashMap<>();
 		for(int i=0; i < nums.length; i++) {
 			int delta = target - nums[i];
@@ -29,5 +15,4 @@ public class TwoSum {
 			
 		return result;   
 	    }
-
 }
