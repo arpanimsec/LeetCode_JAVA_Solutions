fe486c233b91abfb7ce4ef8352456fef774f1fa4
arpanechoindia <70320733+arpanechoindia@users.noreply.github.com>
2021-08-20 12:10:57 +0530

    TwoSum JAVA Solution with main function

--- src/leetCode/TwoSum.java
+++ src/leetCode/TwoSum.java
@@ -1,6 +1,21 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-       int[] result = new int[2];
+package leetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		int[] nums = new int[]{3,3};
+		int target = 6;
+		int[] result = new int[] {100,100};
+		result = twoSum(nums, target);
+		System.out.println(result[0]);
+		System.out.println(result[1]);
+		System.out.println("Hi");
+	}
+	 public static int[] twoSum(int[] nums, int target) {
+	        int[] result = new int[2];
 	    Map<Integer, Integer> map = new HashMap<>();
 		for(int i=0; i < nums.length; i++) {
 			int delta = target - nums[i];
@@ -15,4 +30,5 @@ class Solution {
 			
 		return result;   
 	    }
+
 }
