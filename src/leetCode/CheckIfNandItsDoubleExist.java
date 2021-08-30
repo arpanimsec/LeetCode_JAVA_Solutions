c8d075f7b86d5f7dbbb1b079515f03660e28a592
arpanechoindia <70320733+arpanechoindia@users.noreply.github.com>
2021-08-30 23:31:37 +0530

    Check If N and Its Double Exist JAVA Solution, Runtime: 2 ms, Memory Usage: 38.3 MB,

---
+++ src/leetCode/CheckIfNandItsDoubleExist.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int num: arr) {
+            if((list.contains(num*2)) || (num%2 ==0 &&                                     list.contains(num/2))) {
+                return true;
+            }
+            else {
+                list.add(num);
+            }
+        }
+        return false;
+    }
+}
